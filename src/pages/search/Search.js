15bae7617edd9316257dee2899a14184eeaa5f01
Angelo <angelo.lamonaca@gmail.com>
2021-12-17 14:38:31 +0100

    Collapse all search in a main table

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,49 +1,89 @@
 import * as React from 'react';
-import {Button, Grid} from "@mui/material";
-import {useNavigate} from "react-router";
+import {useEffect, useState} from 'react';
+import {DataGrid} from '@mui/x-data-grid';
+import {getAllPatientsWithAll} from "../../services/patientService";
+import Box from "@mui/material/Box";
 
-export default function GroupOrientation() {
+const columns = [
+    {field: 'patientId', headerName: 'Patient ID', width: 100, editable: false},
+    {field: 'patientName', headerName: 'Patient Name', width: 180, editable: true},
+    {field: 'patientCreatedAt', headerName: 'Patient Created at', width: 160, editable: true},
+    {field: 'studyId', headerName: 'Study ID', width: 100, editable: false},
+    {field: 'studyName', headerName: 'Study Name', width: 180, editable: true},
+    {field: 'studyCreatedAt', headerName: 'Study Created at', width: 160, editable: true},
+    {field: 'seriesId', headerName: 'Series ID', width: 100, editable: false},
+    {field: 'seriesName', headerName: 'Series Name', width: 180, editable: true},
+    {field: 'seriesCreatedAt', headerName: 'SeriesCreated at', width: 160, editable: true},
+    {field: 'fileId', headerName: 'File ID', width: 100, editable: false},
+    {field: 'filePath', headerName: 'File Name', width: 180, editable: true},
+    {field: 'fileCreatedAt', headerName: 'File Created at', width: 160, editable: true},
 
-    const navigate = useNavigate();
-    const goSearchPatients = () => navigate(`/search/patients`);
-    const goSearchStudies = () => navigate(`/search/studies`);
-    const goSearchSeries = () => navigate(`/search/series`);
-    const goSearchFiles = () => navigate(`/search/files`);
+];
+
+export default function DataTable() {
+    const [dataState, setDataState] = useState({
+        data: [{
+            "id": '',
+            "patientId": '',
+            "patientName": "",
+            "patientCreatedAt": "",
+            "studyId": '',
+            "studyName": "",
+            "studyCreatedAt": "",
+            "seriesId": '',
+            "seriesName": "",
+            "seriesCreatedAt": "",
+            "fileId": '',
+            "filePath": "",
+            "fileCreatedAt": "",
+        }]
+    });
+    useEffect(() => {
+        async function fetchData() {
+            const result = await getAllPatientsWithAll();
+            const patients = result.data.data.getAllPatients;
+            let data = [];
+            patients.forEach(patient => {
+                patient.studies?.forEach(study => {
+                    study.series?.forEach(series => {
+                        series.files?.forEach(file => {
+                            data.push({
+                                id: patient.id.toString()+study.id.toString()+series.id.toString()+file.id.toString(),
+                                patientId: patient?.id,
+                                patientName: patient?.name,
+                                patientCreatedAt: new Date(parseInt(patient?.createdAt)).toLocaleString(),
+                                patientUpdatedAt: new Date(parseInt(patient?.updatedAt)).toLocaleString(),
+                                studyId: study?.id,
+                                studyName: study?.studyName,
+                                studyCreatedAt: new Date(parseInt(study?.createdAt)).toLocaleString(),
+                                studyUpdatedAt: new Date(parseInt(study?.updatedAt)).toLocaleString(),
+                                seriesId: series?.id,
+                                seriesName: series?.seriesName,
+                                seriesCreatedAt: new Date(parseInt(series?.createdAt)).toLocaleString(),
+                                seriesUpdatedAt: new Date(parseInt(series?.updatedAt)).toLocaleString(),
+                                fileId: file?.id,
+                                filePath: file?.filePath,
+                                fileCreatedAt: new Date(parseInt(file?.createdAt)).toLocaleString(),
+                                fileUpdatedAt: new Date(parseInt(file?.updatedAt)).toLocaleString()
+                            })
+                        })
+                    })
+                })
+            })
+
+            setDataState({data: data});
+        }
+
+        fetchData();
+    }, []);
     return (
-        <Grid
-            marginTop={'5vh'}
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            component="form"
-            noValidate
-            autoComplete="off">
-            <Button
-                variant="contained"
-                sx={{marginBottom: '5vh', fontSize: '3rem'}}
-                onClick={goSearchPatients}>
-                patients
-            </Button>
-            <Button
-                variant="contained"
-                sx={{marginBottom: '5vh', fontSize: '3rem'}}
-                onClick={goSearchStudies}>
-                studies
-            </Button>
-            <Button
-                variant="contained"
-                sx={{marginBottom: '5vh', fontSize: '3rem'}}
-                onClick={goSearchSeries}>
-                series
-            </Button>
-            <Button
-                variant="contained"
-                sx={{marginBottom: '5vh', fontSize: '3rem'}}
-                onClick={goSearchFiles}>
-                files
-            </Button>
-        </Grid>
+        <Box style={{height: 'calc(100vh - 56px - 56px)', marginBottom: '56px', width: '100%'}}>
+            <DataGrid
+                rowHeight={38}
+                rows={dataState.data}
+                columns={columns}
+                pageSize={12}
+            />
+        </Box>
     );
 }
